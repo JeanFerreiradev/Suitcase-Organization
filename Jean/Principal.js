d81a86441c5f08043bfbed8d0e36e6f87ed4f61e
Jean Ferreira <srferreira2000@hotmail.com>
2023-08-07 17:39:09 -0300

    Atualizado código js página principal

--- Jean/Principal.js
+++ Jean/Principal.js
@@ -7,4 +7,37 @@ function moveBar(clickedDiv) {
 
     bar.style.width = clickedDivRect.width + 'px';
     bar.style.transform = `translateX(${offsetX}px)`;
+
+    /*Mudando o texto explicativo*/
+    var divs = document.getElementsByClassName("tituloArquivo");
+    if (clickedDiv == divs[0]) {
+        var texto = document.querySelector(".texto2");
+        texto.innerText = "Categoria exclusiva para armazenar certidão de nascimento, identidade, CNH, entre outros...";
+    } else if (clickedDiv == divs[1]) {
+        var texto = document.querySelector(".texto2");
+        texto.innerText = "Nesta aba é possível guardar diversos tipos de comprovantes em categorias diferentes.";
+    } else if (clickedDiv == divs[2]) {
+        var texto = document.querySelector(".texto2");
+        texto.innerText = "Guarde aqui suas notas fiscais para acessá-las facilmente.";
+    } else {
+        var texto = document.querySelector(".texto2");
+        texto.innerText = "Nesta seção você consegue criar sua própria categoria para armazenar diversos tipos de arquivos.";
+    }
 }
+
+// function changeTxt(clickedDiv) {
+//     var divs = document.getElementsByClassName("tituloArquivo");
+//     if (clickedDiv == divs[0]) {
+//         var elementoP = clickedDiv.querySelector('p');
+//         elementoP.textContent = "Categoria exclusiva para armazenar certidão de nascimento, identidade, CNH, entre outros...";
+//     } else if (clickedDiv == divs[1]) {
+//         var elementoP = clickedDiv.querySelector('p');
+//         elementoP.textContent = "Nesta aba é possível guardar diversos tipos de comprovantes em categorias diferentes.";
+//     } else if (clickedDiv == divs[2]) {
+//         var elementoP = clickedDiv.querySelector('p');
+//         elementoP.textContent = "Guarde aqui suas notas fiscais para acessá-las facilmente.";
+//     } else {
+//         var elementoP = clickedDiv.querySelector('p');
+//         elementoP.textContent = "Nesta seção você consegue criar sua própria categoria para armazenar diversos tipos de arquivos.";
+//     }
+// }
